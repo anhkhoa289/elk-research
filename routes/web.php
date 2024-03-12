1f377f12ac953968622a839add0c44d86469961b
Khoa Nguyen <khoa.nguyen@stunited.vn>
2024-03-12 09:31:56 +0700

    get elastic detail

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+$elastic_server = "http://127.0.0.1:9200";
+$elastic_user = "elastic";
+$elastic_password = "hKD62=tjwgq8xWQlRli+";
+
+$client = Http::withBasicAuth($elastic_user, $elastic_password)->baseUrl($elastic_server);
+
 Route::get('/', function () {
+    Log::info('Hey');
     return view('welcome');
+});
+
+Route::get('/_aliases', function () use ($client) {
+    $response = $client->get('_aliases');
+    return response()->json($response->object());
 });
